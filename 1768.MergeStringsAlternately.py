4a0cb959a3636e67eabac1ce4b93a32dc82e4369
ericw142 <eric.h.warshawsky@gmail.com>
2024-05-28 14:14:43 -0400

    Create 1768.MergeStringsAlternately.py

---
+++ 1768.MergeStringsAlternately.py
@@ -0,0 +1,62 @@
+# You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1.
+# If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+# Return the merged string.
+
+
+# Example 1:
+
+# Input: word1 = "abc", word2 = "pqr"
+# Output: "apbqcr"
+# Explanation: The merged string will be merged as so:
+# word1:  a   b   c
+# word2:    p   q   r
+# merged: a p b q c r
+
+# Example 2:
+
+# Input: word1 = "ab", word2 = "pqrs"
+# Output: "apbqrs"
+# Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+# word1:  a   b 
+# word2:    p   q   r   s
+# merged: a p b q   r   s
+
+# Example 3:
+
+# Input: word1 = "abcd", word2 = "pq"
+# Output: "apbqcd"
+# Explanation: Notice that as word1 is longer, "cd" is appended to the end.
+# word1:  a   b   c   d
+# word2:    p   q 
+# merged: a p b q c   d
+ 
+
+# Constraints:
+
+#     1 <= word1.length, word2.length <= 100
+#     word1 and word2 consist of lowercase English letters.
+
+
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+        merged = ""
+        totallength = len(word1)
+        if len(word2) > totallength:
+            totallength = len(word2)
+        
+        for x in range(totallength):
+            if (x < len(word1)):
+                merged = merged + word1[x]
+            if (x < len(word2)):
+                merged = merged + word2[x]
+
+        return merged
+
+answer = Solution.mergeAlternately(Solution, "hello", "world")
+print(answer)
